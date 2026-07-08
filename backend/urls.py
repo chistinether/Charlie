ab2f2eb1e14252fa666bbd73479d1b4404365f4f
jerryedens921-boop <jerryedens921@gmail.com>
2026-07-08 13:30:29 +0300

    initial commit

---
+++ backend/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    path("api/", include("charlie.urls")),
+
+    path("api/auth/login/", TokenObtainPairView.as_view(), name="login"),
+    path("api/auth/refresh/", TokenRefreshView.as_view(), name="refresh"),
+]
